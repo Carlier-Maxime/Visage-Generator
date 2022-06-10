2df710902c5d7157716df4f0caffbf6cb45ba7f6
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-06-10 12:00:32 +0200

    add possibility to show balises (BETA)

--- main.py
+++ main.py
@@ -74,12 +74,25 @@ for i in range(nbFace):
     joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
     scene.add(joints_pcl)
     """
-
+    """# Vertices (points of topologie)
     sm = trimesh.creation.uv_sphere(radius=0.002)
     sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
     tfs = np.tile(np.eye(4), (len(vertices), 1, 1))
     tfs[:, :3, 3] = vertices
     vertices_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
     scene.add(vertices_pcl)
+    """
+    """# Balises (change pose params to [70.0*radian, 70.0*radian, 70.0*radian, 0.0, 0.0, 0.0])
+    balises = []
+    for e in vertices:
+        if (e[0]>0.05 and e[2]>-0.03 and e[2]<0.15):
+            balises.append(e)
+    sm = trimesh.creation.uv_sphere(radius=0.002)
+    sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
+    tfs = np.tile(np.eye(4), (len(balises), 1, 1))
+    tfs[:, :3, 3] = balises
+    balises_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+    scene.add(balises_pcl)
+    """
 
     pyrender.Viewer(scene, use_raymond_lighting=True)
