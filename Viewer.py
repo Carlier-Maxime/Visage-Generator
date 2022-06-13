6f15af067ec8f24bcb07c0487059b21d2b669a92
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-06-13 14:48:48 +0200

    add button to show vertices

--- Viewer.py
+++ Viewer.py
@@ -5,16 +5,27 @@ from config import nbFace,device
 
 class Viewer(pyrender.Viewer):
     def __init__(self, vertice, landmark, faces, show_joints=False, show_vertices=False):
+        self._vertice = vertice
+        self._landmark = landmark
+        self._faces = faces
+        self._show_vertices = False
+        self._show_joints = False
+        self._scene = pyrender.Scene()
+        self._scene.add_node(self.getVisage(0))
+        self._index = 0
+
+        #vertices node
+        vertices = self._vertice[self._index]
+        sm = trimesh.creation.uv_sphere(radius=0.002)
+        sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
+        tfs = np.tile(np.eye(4), (len(vertices), 1, 1))
+        tfs[:, :3, 3] = vertices
+        vertices_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+        self._verticesNode = pyrender.Node("vertices",mesh=vertices_pcl)
+
+        pyrender.Viewer.__init__(self, self._scene, use_raymond_lighting=True, run_in_thread=True)
         for i in range(nbFace):
-            vertices = vertice[i].detach().to(device).numpy().squeeze()
-            joints = landmark[i].detach().to(device).numpy().squeeze()
-            vertex_colors = np.ones([vertices.shape[0], 4]) * [0.925, 0.72, 0.519, 1.0]
-
-            tri_mesh = trimesh.Trimesh(vertices, faces, vertex_colors=vertex_colors)
-            mesh = pyrender.Mesh.from_trimesh(tri_mesh)
-            scene = pyrender.Scene()
-            scene.add(mesh)
-
+            self.render_lock.acquire()
             if show_joints:
                 # Joints (landmarks)
                 sm = trimesh.creation.uv_sphere(radius=0.002)
@@ -22,20 +33,15 @@ class Viewer(pyrender.Viewer):
                 tfs = np.tile(np.eye(4), (len(joints), 1, 1))
                 tfs[:, :3, 3] = joints
                 joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
-                scene.add(joints_pcl)
+                self._scene.add(joints_pcl)     
+            self.render_lock.release()
             if show_vertices:
-                # Vertices (points of topologie)
-                sm = trimesh.creation.uv_sphere(radius=0.002)
-                sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
-                tfs = np.tile(np.eye(4), (len(vertices), 1, 1))
-                tfs[:, :3, 3] = vertices
-                vertices_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
-                scene.add(vertices_pcl)
-            
-            pyrender.Viewer.__init__(self, scene, use_raymond_lighting=True)
+                self.showVertices()
 
     def on_key_press(self, symbol, modifiers):
         pyrender.Viewer.on_key_press(self,symbol,modifiers)
+        if symbol == 118: # show vertices
+            self.showVertices()
         if symbol == 65362: # Up Arrow
             pass
         if symbol == 65364: # Down Arrow
@@ -44,3 +50,23 @@ class Viewer(pyrender.Viewer):
             pass
         if symbol == 65363: # Right Arrow
             pass
+
+    def showVertices(self):
+        self.render_lock.acquire()
+        if not self._show_vertices:
+            self._scene.add_node(self._verticesNode)
+            self._show_vertices = True
+        else:
+            self._scene.remove_node(self._verticesNode)
+            self._scene.add_node(self.getVisage(self._index))
+            self._show_vertices = False
+        self.render_lock.release()
+
+    def getVisage(self,i):
+        vertices = self._vertice[i].detach().to(device).numpy().squeeze()
+        joints = self._landmark[i].detach().to(device).numpy().squeeze()
+        vertex_colors = np.ones([vertices.shape[0], 4]) * [0.925, 0.72, 0.519, 1.0]
+
+        tri_mesh = trimesh.Trimesh(vertices, self._faces, vertex_colors=vertex_colors)
+        mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+        return pyrender.Node("Visage",mesh=mesh)
