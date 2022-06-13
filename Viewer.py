35debcbe94fd8c81228bbc1c41272b1354e53223
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-06-13 15:07:54 +0200

    add button for show joints

--- Viewer.py
+++ Viewer.py
@@ -11,7 +11,8 @@ class Viewer(pyrender.Viewer):
         self._show_vertices = False
         self._show_joints = False
         self._scene = pyrender.Scene()
-        self._scene.add_node(self.getVisage(0))
+        self._visage = self.setVisage(0)
+        self._scene.add_node(self._visage)
         self._index = 0
 
         #vertices node
@@ -59,7 +60,6 @@ class Viewer(pyrender.Viewer):
             self._show_vertices = True
         else:
             self._scene.remove_node(self._verticesNode)
-            self._scene.add_node(self.getVisage(self._index))
             self._show_vertices = False
         self.render_lock.release()
 
@@ -70,15 +70,23 @@ class Viewer(pyrender.Viewer):
             self._show_joints = True
         else:
             self._scene.remove_node(self._jointsNode)
-            self._scene.add_node(self.getVisage(self._index))
             self._show_joints = False
         self.render_lock.release()
 
-    def getVisage(self,i):
+    def setVisage(self,i):
         vertices = self._vertice[i].detach().to(device).numpy().squeeze()
         joints = self._landmark[i].detach().to(device).numpy().squeeze()
         vertex_colors = np.ones([vertices.shape[0], 4]) * [0.925, 0.72, 0.519, 1.0]
 
         tri_mesh = trimesh.Trimesh(vertices, self._faces, vertex_colors=vertex_colors)
         mesh = pyrender.Mesh.from_trimesh(tri_mesh)
-        return pyrender.Node("Visage",mesh=mesh)
+        self._visage = pyrender.Node("Visage",mesh=mesh)
+
+    def on_close(self):
+        if (self._index<nbFace-1):
+            self._index = self._index+1
+            self.render_lock.acquire()
+            self.
+            self.render_lock.release()
+        else:
+            pyrender.Viewer.on_close(self)
