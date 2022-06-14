d24f8489b4ffc5dea1bfa382dc18beb2e05d66db
Maxime Carlier <28m06c02@gmail.com>
2022-06-14 16:36:14 +0200

    genDirectionnalMatrix v1

--- util.py
+++ util.py
@@ -83,3 +83,47 @@ def selectRandomBalises():
     balises = balises[0:150]    
 
     np.save('balises.npy',balises)
+
+def genDirectionnalMatrix(vertices):
+    m = []
+    for i in range(len(vertices)):
+        indexD = [-1,-1,-1,-1,-1,-1]
+        distD = [-1,-1,-1,-1,-1,-1]
+        dist1D = [-1,-1,-1,-1,-1,-1]
+        coo = vertices[i]
+        for j in range(len(vertices)):
+            coo2 = vertices[j]
+            dist = np.sqrt((coo2[0]-coo[0])**2+(coo2[1]-coo[1])**2+(coo2[2]-coo[2])**2)
+            ind = getIndexForD(indexD,distD,dist,coo,coo2)
+            if ind[0]!=-1:
+                indexD[ind[0]] = j
+                distD[ind[0]] = dist
+                dist1D[ind[0]] = ind[1]
+        m.append(indexD)
+        print(str(i)+"/"+str(len(vertices)-1))
+    np.save("directionnalMatrix.npy",m)
+
+
+def getIndexForD(indexD,distD,dist1D,dist,coo,coo2):
+    for i in range(len(indexD)):
+        if indexD[i]==-1:
+            return i
+    best = [-1,-1]
+    for i in range(len(distD)):
+        if dist<distD:
+            n = coo2[0]-coo[0]
+            if i==0 and (n)>dist1D[i] and (n)<=0:
+                best = [i,n]
+            if i==0 and (n)<dist1D[i] and (n)>=0:
+                best = [i,n]
+            n = coo2[1]-coo[1]
+            if i==0 and (n)>dist1D[i] and (n)<=0:
+                best = [i,n]
+            if i==0 and (n)<dist1D[i] and (n)>=0:
+                best = [i,n]
+            n = coo2[2]-coo[2]
+            if i==0 and (n)>dist1D[i] and (n)<=0:
+                best = [i,n]
+            if i==0 and (n)<dist1D[i] and (n)>=0:
+                best = [i,n]
+    return best
