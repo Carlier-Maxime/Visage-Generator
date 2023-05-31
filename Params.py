d2a80341831fff1d64eefecea54f9cece0df1070
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-05-31 11:53:23 +0200

    fix ParamsGenerator

--- Params.py
+++ Params.py
@@ -55,27 +55,25 @@ class MultiParamsGenerator(BaseParamsGenerator):
         return cls(params[::3].to(torch.int), params[1::3], params[2::3], device)
 
     def generate(self, size) -> torch.Tensor:
-        if isinstance(size, int): assert size%sum(self.nb_params)==0
-        else: assert size[-1]==sum(self.nb_params), f'a size format is not good, size:{size} size[-1]=={sum(self.nb_params)}'
+        assert size[-1]==self.nbParams(), f'a size format is not good, size:{size} size[-1]=={self.nbParams()}'
+        size=list(size)
         params = []
-        size = list(size)
         for i in range(len(self.nb_params)):
-            if isinstance(size, int): size=self.nb_params[i]
-            else: size[-1]=self.nb_params[i]
+            size[-1]=self.nb_params[i]
             params.append(self.params_generators[i].generate(size))
         return torch.cat(params, dim=1)
     
     def one(self) -> torch.Tensor:
-        return self.generate(sum(self.nb_params))
+        return self.get(1).flatten()
     
     def zeros(self, count:int=-1) -> torch.Tensor:
-        if count==-1: size = sum(self.nb_params)
-        else: size = [count, sum(self.nb_params)]
+        if count==-1: size = self.nbParams()
+        else: size = [count, self.nbParams()]
         return torch.zeros(size, device=self.device)
     
     def get(self, count:int, same:bool=False) -> torch.Tensor:
-        params = self.generate((1 if same else count, sum(self.nb_params)))
+        params = self.generate((1 if same else count, self.nbParams()))
         return params.repeat(count, 1) if same else params
     
     def nbParams(self) -> int:
-        return self.nb_params.sum()
+        return int(self.nb_params.sum())
